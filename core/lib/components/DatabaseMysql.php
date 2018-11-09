3c579ecea55184969917a68250246f5bf1f5926f
tvitas <vitalijus.trainys@lsmuni.lt>
2018-11-09 11:38:58 +0200

    Final

--- core/lib/components/DatabaseMysql.php
+++ core/lib/components/DatabaseMysql.php
@@ -1,25 +1,8 @@
 <?php
 function connectDb()
 {
-    $connection = array('connMsg' => 'Failrue',
-        'dbHandler' => false,
-        'connError' => 'Unknown error',
-    );
-
-    $db = false;
-
     if (defined('DB_HOST') and defined('DB_USER') and defined('DB_PASSWORD') and defined('DB_DATA')) {
-        $db = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATA);
-    } else {
-        $connection['connMsg'] = 'Missing connection parameters.';
+        return new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATA);
     }
-
-    if (!$db) {
-        $connection['connError'] = $db->connect_errno() . $db->connect_error();
-    } else {
-        $connection['connMsg']   = 'Connected';
-        $connection['dbObject']  = $db;
-        $connection['connError'] = 'No error';
-    }
-    return $connection;
+    return false;
 }
--- core/lib/components/Url.php
+++ core/lib/components/Url.php
@@ -1,7 +1,7 @@
 <?php
 function getBaseUrl()
 {
-    if ('Off' === IS_MOD_REWRITE) {
+    if ('Off' === IS_MOD_REWRITE ) {
         return BASE_URL . INDEX_NAME . '/';
     }
     return BASE_URL;
